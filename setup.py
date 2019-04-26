005f0ff131bd70a806b692c7f164ceee7184d57e
Dave <david.schuster@gmail.com>
2019-04-26 13:14:04 -0500

    gui skeleton

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="n2yo",
-    version="0.0.4",
+    version="0.0.5",
     author="David Schuster",
     author_email="David.Schuster@gmail.com",
     description="Python wrapper for the N2YO API",
